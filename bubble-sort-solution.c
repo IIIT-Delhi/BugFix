ffcab90ced47823031f77e6c78c0f4d7c513635e
Vikranth3140 <vikranth22570@iiitd.ac.in>
2023-12-31 20:03:20 +0530

    Added bubble-sort solution

---
+++ bubble-sort-solution.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Function to perform bubble sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            // Corrected: Use > for ascending order
+            if (arr[j] > arr[j + 1]) {
+                // Corrected: Swap elements correctly
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Driver program
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Corrected: Use the correct function name
+    bubbleSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
